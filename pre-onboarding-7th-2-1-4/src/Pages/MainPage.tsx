4700db3bd62433e84eeee0d683f42f9d522592a8
ganeodolu <ganeodolu@gmail.com>
2022-11-03 12:59:27 +0900

    feat: 차량 목록 표시

--- pre-onboarding-7th-2-1-4/src/Pages/MainPage.tsx
+++ pre-onboarding-7th-2-1-4/src/Pages/MainPage.tsx
@@ -1,7 +1,22 @@
+import CarList from "@/Components/CarList/CarList";
+import Button from "@/Components/Common/Button";
+import { SEGMENT } from "@/lib/constants/constants";
 import React from "react";
 
 const MainPage = () => {
-  return <div>MainPage</div>;
+  return (
+    <>
+      <p>MainPage</p>
+      <nav>
+        <Button>전체</Button>
+        <Button>{SEGMENT.E}</Button>
+        <Button>{SEGMENT.D}</Button>
+        <Button>{SEGMENT.C}</Button>
+        <Button>{SEGMENT.SUV}</Button>
+      </nav>
+      <CarList />
+    </>
+  );
 };
 
 export default MainPage;
