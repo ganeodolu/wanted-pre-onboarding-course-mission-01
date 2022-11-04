2349c4478209353956d07ddd39f3d656d8f717aa
ganeodolu <ganeodolu@gmail.com>
2022-11-04 13:34:07 +0900

    feat: react query, devtools, theme, globalStyles 적용

--- pre-onboarding-7th-2-1-4/src/index.tsx
+++ pre-onboarding-7th-2-1-4/src/index.tsx
@@ -1,16 +1,36 @@
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import { ReactQueryDevtools } from "@tanstack/react-query-devtools/build/lib/devtools";
+import { ThemeProvider } from "styled-components";
 import React from "react";
 import ReactDOM from "react-dom/client";
-import "./index.css";
-import App from "./App";
-import reportWebVitals from "./reportWebVitals";
 import { BrowserRouter } from "react-router-dom";
+import App from "./App";
+import "./index.css";
+import { GlobalStyles, theme } from "./lib/styles";
+import reportWebVitals from "./reportWebVitals";
+
+const queryClient = new QueryClient({
+  defaultOptions: {
+    queries: {
+      staleTime: 1000 * 60 * 5,
+      cacheTime: 1000 * 60 * 10,
+      refetchOnWindowFocus: false
+    }
+  }
+});
 
 const root = ReactDOM.createRoot(
   document.getElementById("root") as HTMLElement
 );
 root.render(
   <BrowserRouter>
-    <App />
+    <QueryClientProvider client={queryClient}>
+      <ReactQueryDevtools initialIsOpen={false} />
+      <ThemeProvider theme={theme}>
+        <GlobalStyles />
+        <App />
+      </ThemeProvider>
+    </QueryClientProvider>
   </BrowserRouter>
 );
 
