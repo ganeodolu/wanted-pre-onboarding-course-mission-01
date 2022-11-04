22e58b03064681f0a5b8cf54d683b458a8021fc9
ganeodolu <ganeodolu@gmail.com>
2022-11-04 20:52:42 +0900

    fix: 환경설정 수정

--- pre-onboarding-7th-2-1-4/src/index.tsx
+++ pre-onboarding-7th-2-1-4/src/index.tsx
@@ -1,9 +1,9 @@
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
-import { ReactQueryDevtools } from "@tanstack/react-query-devtools/build/lib/devtools";
-import { ThemeProvider } from "styled-components";
+import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
 import React from "react";
 import ReactDOM from "react-dom/client";
 import { BrowserRouter } from "react-router-dom";
+import { ThemeProvider } from "styled-components";
 import App from "./App";
 import "./index.css";
 import { GlobalStyles, theme } from "./lib/styles";
