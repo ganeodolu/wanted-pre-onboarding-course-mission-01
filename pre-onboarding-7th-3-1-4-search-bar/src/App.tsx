c4f4b950ee488ea6567c64ae52948a012a3e446e
ganeodolu <ganeodolu@gmail.com>
2022-11-10 11:28:56 +0900

    feat: 라우터 적용

--- pre-onboarding-7th-3-1-4-search-bar/src/App.tsx
+++ pre-onboarding-7th-3-1-4-search-bar/src/App.tsx
@@ -1,25 +1,19 @@
 import React from 'react';
-import logo from './logo.svg';
+import { Route, Routes } from 'react-router-dom';
 import './App.css';
+import Layout from './Components/Layout';
+import DetailPage from './Pages/DetailPage';
+import HomePage from './Pages/HomePage';
+
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Routes>
+      <Route element={<Layout />}>
+        <Route index element={<HomePage />} />
+        <Route path="/detail/:id" element={<DetailPage />} />
+      </Route>
+    </Routes>
   );
 }
 
