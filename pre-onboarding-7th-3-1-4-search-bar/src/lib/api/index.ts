4665a20c76e435a162fb7e7f338c6e743996ff14
ganeodolu <ganeodolu@gmail.com>
2022-11-14 00:00:38 +0900

    feat: 환경설정

--- pre-onboarding-7th-3-1-4-search-bar/src/lib/api/index.ts
+++ pre-onboarding-7th-3-1-4-search-bar/src/lib/api/index.ts
@@ -8,41 +8,39 @@ const instance = axios.create({
 const cache: any = {};
 
 const request = async (url: string) => {
-  console.log(cache)
   const cacheValue = await cache[url];
-  if(cacheValue){
-    return cacheValue
+  if (cacheValue) {
+    return cacheValue;
   }
-	try {
-		const response = await instance(url);
-    console.info("calling api")
-    console.log(response)
-		const responseCode = response.status;
-		if (responseCode === 200) {
-      cache[url] = await response.data; 
-			return cache[url];
-		} else {
-			switch (responseCode) {
-				case 400:
-					console.log(`CODE: ${responseCode} Bad Request`);
-					break;
-				case 404:
-					console.log(`CODE: ${responseCode} Not Found`);
-					break;
-				case 500:
-					console.log(`CODE: ${responseCode} Internal Server Error`);
-					break;
-			}
-		}
-	} catch (error) {
-		throw new Error("API 응답이 없습니다.");
-	}
+  try {
+    const response = await instance(url);
+    console.info("calling api");
+    const responseCode = response.status;
+    if (responseCode === 200) {
+      cache[url] = await response.data;
+      return cache[url];
+    } else {
+      switch (responseCode) {
+        case 400:
+          console.log(`CODE: ${responseCode} Bad Request`);
+          break;
+        case 404:
+          console.log(`CODE: ${responseCode} Not Found`);
+          break;
+        case 500:
+          console.log(`CODE: ${responseCode} Internal Server Error`);
+          break;
+      }
+    }
+  } catch (error) {
+    throw new Error("API 응답이 없습니다.");
+  }
 };
 
 const API = {
   getKeyword: (keyword: string) => {
-    return request(`/sick?q=${keyword}`)
+    return request(`/sick?q=${keyword}`);
   }
-}
+};
 
-export default API
+export default API;
